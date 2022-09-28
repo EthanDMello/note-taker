e6adb3d240d86989c97ea1bb4a4adb39af5e4667
TheBiggestThereIs <107923914+EthanDMello@users.noreply.github.com>
2022-09-28 10:36:47 +0100

    server routes added

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+// const util = require("util");
+
+// Helper method for generating unique ids
+// const uuid = require("./helpers/uuid");
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET Route for feedback page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// Promise version of fs.readFile
+// const readFromFile = util.promisify(fs.readFile);
+
+// GET Route for retrieving all the feedback
+app.get("/api/notes", (req, res) => {
+  //   console.info(`${req.method} request received for feedback`);
+
+  fs.readFile("./db/db.json", (err, note) => {
+    res.json(JSON.parse(note));
+  });
+  //   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+
+  readAndAppend(req.body, "./db/db.json");
+});
+// POST Route for submitting feedback
+// app.post("/api/notes", (req, res) => {
+//   // Log that a POST request was received
+//   console.info(`${req.method} request received to submit feedback`);
+
+//   // Destructuring assignment for the items in req.body
+//   const { email, feedbackType, feedback } = req.body;
+
+//   // If all the required properties are present
+//   if (email && feedbackType && feedback) {
+//     // Variable for the object we will save
+//     const newFeedback = {
+//       email,
+//       feedbackType,
+//       feedback,
+//       feedback_id: uuid(),
+//     };
+
+//     readAndAppend(newFeedback, "./db/feedback.json");
+
+//     const response = {
+//       status: "success",
+//       body: newFeedback,
+//     };
+
+//     res.json(response);
+//   } else {
+//     res.json("Error in posting feedback");
+//   }
+// });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
